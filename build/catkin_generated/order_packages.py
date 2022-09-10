d2324217cdcaecb135e9416195f42368050dc8a6
miracle04s <109651186+miracle04s@users.noreply.github.com>
2022-09-10 21:32:34 +0800

    Add files via upload

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ss/simulation02/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ss/simulation02/devel_isolated/moveit_config;/home/ss/simulation02/devel_isolated/demo02_urdf_gazebo;/home/ss/simulation02/devel;/opt/ros/noetic'.split(';') if '/home/ss/simulation02/devel_isolated/moveit_config;/home/ss/simulation02/devel_isolated/demo02_urdf_gazebo;/home/ss/simulation02/devel;/opt/ros/noetic' != '' else []
