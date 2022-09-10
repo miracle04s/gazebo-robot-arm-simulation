3ab19713d99508d5b77ae693d28bd54ed5ded67a
miracle04s <109651186+miracle04s@users.noreply.github.com>
2022-09-10 21:34:21 +0800

    Add files via upload

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/ss/simulation02/src
+# Build directory: /home/ss/simulation02/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("moveit_config")
+subdirs("arm_publish")
+subdirs("arm_sub")
+subdirs("demo02_urdf_gazebo")
---
+++ build/moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ss/simulation02/src/moveit_config
+# Build directory: /home/ss/simulation02/build/moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
