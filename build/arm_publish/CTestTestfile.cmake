bb08108749c807c0b0c9a60e20e3a4ce046fef5c
miracle04s <109651186+miracle04s@users.noreply.github.com>
2022-09-10 21:29:27 +0800

    Add files via upload

---
+++ build/arm_publish/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ss/simulation02/src/arm_publish
+# Build directory: /home/ss/simulation02/build/arm_publish
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
